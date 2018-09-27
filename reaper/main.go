5d3b966918f68f701a43905dfcd3f89149fce888
Ted Hahn <ted@lifeonair.com>
2018-09-26 17:39:07 -0700

    Sort

--- reaper/main.go
+++ reaper/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"time"
 
 	core "k8s.io/api/core/v1"
 	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -58,3 +59,30 @@ func podIsUnready(p core.Pod) bool {
 }
 
 type unreadyPodList []*core.Pod
+
+// These functions conform to the sort interface
+func (l unreadyPodList) Len() int {
+	return len(l)
+}
+
+func (l *unreadyPodList) Swap(i, j int) {
+	p := (*l)[i]
+	(*l)[i] = (*l)[j]
+	(*l)[j] = p
+}
+
+// Our unreadyPodList sorts by the time the pod went unready
+func (l *unreadyPodList) Less(i, j int) bool {
+	unreadyTime := func(p *core.Pod) *meta.Time {
+		for _, c := range p.Status.Conditions {
+			if c.Type == Ready {
+				return &c.LastTransitionTime
+			}
+		}
+		// This shouldn't happen, but if it does, ignore the pod.
+		log.Println("Unable to determine last unreadiness time of pod ", p.Name)
+		return &meta.Time{time.Now()}
+	}
+
+	return unreadyTime((*l)[i]).Before(unreadyTime((*l)[j]))
+}
